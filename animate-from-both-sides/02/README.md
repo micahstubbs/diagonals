21806dd9fd9be875d8a1387c28cef56ec5806244
Micah Stubbs <micah.stubbs@gmail.com>
2018-09-15 14:57:19 -0700

    add iteration 02 that will show diagonals from top left to bottom right

---
+++ animate-from-both-sides/02/README.md
@@ -0,0 +1,28 @@
+this iteration shows the diagonals that from from top-left to bottom-right
+
+---
+
+this iteration makes the code nice to work with, for my subjective definition of "nice"
+
+- js in index.js file
+- prettier format js
+- update js with lebab
+- single quotes in html
+- no line breaks in html
+
+an iteration on https://bl.ocks.org/tophtucker/45e8112e70ea9192c8d666dba6fe79fc
+
+---
+
+Someone just wanted to make a grid of squares animate in diagonally. This takes a series of numbers and maps them to rows and columns using [triangular numbers](https://en.wikipedia.org/wiki/Triangular_number). (It might’ve been more straightforward to just [invert the Cantor pairing function](https://en.wikipedia.org/wiki/Pairing_function#Inverting_the_Cantor_pairing_function).)
+
+I added little fractions text, so it doubles as a visual proof of [the countability of the rationals](http://www.homeschoolmath.net/teaching/rational-numbers-countable.php)! And then I made two, so it doubles as a visual proof of the fact that [the sum of consecutive triangular numbers is a square number](https://en.wikipedia.org/wiki/Triangular_number#Relations_to_other_figurate_numbers)!
+
+Refresh for a different pair of consecutive triangle numbers.
+
+See also:
+
+- [Straight diagonal](http://bl.ocks.org/tophtucker/07acfe8e14559532fd226c954783cd7d) using comparator on existing data
+- [Stepped diagonal](http://bl.ocks.org/tophtucker/f093228a443bf38af53c69722e2c40f4) using Cantor pairing comparator on existing data
+- [Zigzag diagonal](http://bl.ocks.org/tophtucker/a18d11836a353730cef46bd005a82851) using alternating Cantor pairing comparator on existing data
+- [Generating by triangular numbers](http://bl.ocks.org/tophtucker/45e8112e70ea9192c8d666dba6fe79fc)
---
+++ animate-from-both-sides/02/index.js
@@ -0,0 +1,87 @@
+const pageWidth = 960
+const gridSize = 960 / Math.ceil(Math.random() * 24)
+const triangularNumber = 960 / gridSize - 1
+const delay = 50
+
+// Render warm upper larger triangle
+renderTriangle(
+  d3.select('body').append('section'),
+  triangularNumber + 1,
+  d3.scaleWarm().domain([0, getTriangularNumber(triangularNumber + 1) * 2]),
+  d => d
+)
+
+// Render cool lower smaller triangle
+renderTriangle(
+  d3.select('body').append('section'),
+  triangularNumber,
+  d3.scaleCool().domain([getTriangularNumber(triangularNumber) * 2, 0]),
+  d => triangularNumber - d
+)
+
+function renderTriangle(selection, triangularNumber, color, offsetter) {
+  // offsetter just lets us choose whether to count from upper left or bottom right
+  if (offsetter === undefined) offsetter = d => d
+  if (color === undefined) color = d3.scaleRainbow()
+
+  selection
+    .selectAll('div')
+    .data(
+      d3
+        .range(getTriangularNumber(triangularNumber))
+        .map(getUpperTriangleCoordinates)
+    )
+    .enter()
+    .append('div')
+    .style('width', `${gridSize}px`)
+    .style('height', `${gridSize}px`)
+    .style('background-color', (d, i) => color(i))
+    .style('font-size', `${gridSize / 4}px`)
+    .style('line-height', `${gridSize}px`)
+    .style('left', (d, n) => `${gridSize * offsetter(d.column)}px`)
+    .style('top', (d, n) => `${gridSize * offsetter(d.row)}px`)
+    .style('opacity', 0)
+    .text((d, n) => `${d.row + 1}/${d.column + 1}`)
+    .transition()
+    .delay((d, n) => n * delay)
+    .style('opacity', 1)
+}
+
+// https://en.wikipedia.org/wiki/Triangular_number
+function getTriangularNumber(n) {
+  return (n * (n + 1)) / 2
+}
+
+function getTriangularNumberInverse(n) {
+  return (1 / 2) * (-1 + Math.sqrt(8 * n + 1))
+}
+
+/*
+  0 2 5
+  1 4 
+  3
+*/
+function getUpperTriangleCoordinates(n) {
+  const i = Math.floor(getTriangularNumberInverse(n))
+  return {
+    n,
+    row: i - (n - getTriangularNumber(i)),
+    column: n - getTriangularNumber(i)
+  }
+}
+
+// Below, an unused alternative!
+
+/*
+  0
+  1 2
+  3 4 5
+*/
+function getLowerTriangleCoordinates(n) {
+  const i = Math.floor(getTriangularNumberInverse(n))
+  return {
+    n,
+    row: i,
+    column: n - getTriangularNumber(i)
+  }
+}
