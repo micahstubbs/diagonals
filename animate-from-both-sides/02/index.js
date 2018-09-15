f838e5a14d8541a2b0e0adffdf208c80a2653391
Micah Stubbs <micah.stubbs@gmail.com>
2018-09-15 15:55:49 -0700

    add WIP state. not quite there yet

--- animate-from-both-sides/02/index.js
+++ animate-from-both-sides/02/index.js
@@ -1,5 +1,6 @@
 const pageWidth = 960
-const gridSize = 960 / Math.ceil(Math.random() * 24)
+const gridSize = 960 / 3
+// const gridSize = 960 / Math.ceil(Math.random() * 12)
 const triangularNumber = 960 / gridSize - 1
 const delay = 50
 
@@ -8,7 +9,7 @@ renderTriangle(
   d3.select('body').append('section'),
   triangularNumber + 1,
   d3.scaleWarm().domain([0, getTriangularNumber(triangularNumber + 1) * 2]),
-  d => d
+  d => triangularNumber - d
 )
 
 // Render cool lower smaller triangle
@@ -16,7 +17,7 @@ renderTriangle(
   d3.select('body').append('section'),
   triangularNumber,
   d3.scaleCool().domain([getTriangularNumber(triangularNumber) * 2, 0]),
-  d => triangularNumber - d
+  d => d
 )
 
 function renderTriangle(selection, triangularNumber, color, offsetter) {
@@ -29,7 +30,7 @@ function renderTriangle(selection, triangularNumber, color, offsetter) {
     .data(
       d3
         .range(getTriangularNumber(triangularNumber))
-        .map(getLowerTriangleCoordinates)
+        .map(getUpperTriangleCoordinatesDiagonalOrder)
     )
     .enter()
     .append('div')
@@ -56,12 +57,41 @@ function getTriangularNumberInverse(n) {
   return (1 / 2) * (-1 + Math.sqrt(8 * n + 1))
 }
 
+// we use this one.
+
+/*
+  5
+  2 4
+  0 1 3
+*/
+function getLowerTriangleCoordinatesDiagonalOrder(n) {
+  const i = Math.floor(getTriangularNumberInverse(n))
+  const intermediateValues = {
+    i,
+    tni: getTriangularNumber(i),
+    tnn: getTriangularNumber(n),
+    tn: triangularNumber,
+    tnnLessOne: getTriangularNumber(n) - 1
+  }
+  const coords = {
+    n,
+    row: triangularNumber - n - getTriangularNumber(i),
+    column: n - getTriangularNumber(i)
+  }
+  console.log('intermediateValues', intermediateValues)
+  console.log('coords', coords)
+  console.log('~~~~~~~~~~')
+  return coords
+}
+
+// an unused alternative!
+
 /*
   0 2 5
   1 4 
   3
 */
-function getUpperTriangleCoordinates(n) {
+function getUpperTriangleCoordinatesDiagonalOrder(n) {
   const i = Math.floor(getTriangularNumberInverse(n))
   return {
     n,
@@ -70,14 +100,14 @@ function getUpperTriangleCoordinates(n) {
   }
 }
 
-// Below, an unused alternative!
+// an unused alternative!
 
 /*
   0
   1 2
   3 4 5
 */
-function getLowerTriangleCoordinates(n) {
+function getLowerTriangleCoordinatesRowOrder(n) {
   const i = Math.floor(getTriangularNumberInverse(n))
   return {
     n,
